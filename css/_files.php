b2faa9ba3d1ca0113dc95de45cf9c4f59677f362
Thomas Muguet <t.muguet@thomasmuguet.info>
2013-08-17 00:56:33 +0200

    Update font-awesome to v3.2.1

--- css/_files.php
+++ css/_files.php
@@ -1,7 +1,8 @@
 <?php
 $_files = array(
     "fluid960gs/reset.css",
-    "smoothness/jquery-ui-1.10.2.custom.css",
+    "font-awesome.min.css",
+    "jquery-ui-bootstrap/jquery-ui-1.10.2.custom.css",
     "grid.css",
     "owaka.css",
 );
